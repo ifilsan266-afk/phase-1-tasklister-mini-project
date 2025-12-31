30699acbfeca3d93c89a5653c8dbb8452e949c7a
“filsan” <dministrator@DESKTOP-VKRMBR2.localdomain>
2025-12-31 08:19:30 +0300

    Add task to list on form submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", (event) => {
+  const task = document.getElementById("new-task-description").value;
+  buildToDo(task);
 });
+
+function buildToDo(task) {
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li");
+  li.textContent = task;
+  ul.appendChild(li);
+}
